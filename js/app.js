4cd5f684cbde80e79c729c7728a53ce8b08ddfaf
magedashuriqi <mageda.shuriqi@gmail.com>
2021-02-03 12:27:49 +0200

    301-exam

---
+++ js/app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var courseArray=['Art','Arabic','English','Math','Science'];
+studentTotal=[];
+function Students (name,course){
+    this.name=name;
+    this.grades=0;
+    this.course=course;
+    studentTotal.push(this)
+}
+
+//-----------------
+function generateGrade(min,max){
+    return Math.floor( Math.random()*((max-min+1)+min))
+}
+//generateGrade(0,100);
+//------------
+//create table
+
+var tableInfo = document.getElementById('first-Row');
+
+function addStudent(event){
+    document.getElementById('submit')
+    button.addEventListener('submit',submitter())
+    console.log(event);
+}
+ addStudent();
+
+function submitter(){
+    var mainTable=document.getElementById('s-table');
+    var studentRow=document.createElement('tr')
+    mainTable.appendChild(studentRow);
+    stuName=document.createElement('td');
+    stuName.textcontent=studentTotal[0];
+    stuName.appendChild(studentRow);
+    var stugrade=document.createElement('td');
+    stugrade.textcontent=generateGrade(0,100);
+    stugrade.appendChild(studentRow);
+    var stuCourse=document.createElement('td');
+    stuCourse.textcontent=
+    studentRow.appendChild(stuCourse)
+}
